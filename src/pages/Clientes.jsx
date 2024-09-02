f9180a00e365f3c313f504d31cf0851a6ed7b8da
LeoMartinez013 <105740782+LeoMartinez013@users.noreply.github.com>
2024-09-02 14:48:52 -0300

    02/09/2024 1
    ajustes gerais;
    container para registrar clientes;
    layout test Tecnicos.

--- src/pages/Clientes.jsx
+++ src/pages/Clientes.jsx
@@ -5,6 +5,10 @@ import options from '../assets/options.png'
 import '../styles/clientes.css'
 
 function Clientes() {
+
+    async function addCliente() {
+        changeRegClienteContainer()
+    }
     async function changeNav() {
         const sideNav = document.getElementById("sideNavClientes")
         const button = document.getElementById("bttNav")
@@ -19,6 +23,17 @@ function Clientes() {
             button.src = navFechar
         }
     }
+    async function changeRegClienteContainer() {
+        const sombra = document.getElementById("sombra")
+        const contRegCliente = document.getElementById("containerNovoCliente")
+        if (sombra.style.display == 'none') {
+            sombra.style.display = 'flex'
+            contRegCliente.style.display = 'grid'
+        } else {
+            document.getElementById("sombra").style.display = 'none',
+            document.getElementById("containerNovoCliente").style.display = 'none'
+        }
+    }
     return (
         <>
         <nav id='navClientes'>
@@ -28,22 +43,20 @@ function Clientes() {
             </Link>
         </nav>
         <div id="sideNavClientes" className="goTo">
-            <Link className="links" to="/tecnicos">tenicos</Link>
             <Link className="links" to="/home">home</Link>
+            <Link className="links" to="/tecnicos">tenicos</Link>
             <Link className="links" to="/ordens">ordens de serviços</Link>
         </div>
         <main id='mainClientes'>
-            {/* Filtragem 
-                NOME
-                QTD ENDEREÇOS
-                QT
-            */}
-            <section>
+            {/* Listagem */}
+            <section id='containerListClientes'>
                 <div id='tabelaClientes'>
                     <div id='cabecalho'>
                         <div id='cNome'>nome</div>
                         <div id='cCNPJ'>CNPJ</div>
-                        <div id='adicionarCliente'>+</div>
+                        <div id='adicionarCliente'>
+                            <button onClick={changeRegClienteContainer}>novo cliente</button>
+                        </div>
                     </div>
                     <div id='listClientes'>
                         <div id='cliente1' className='clientes'>
@@ -59,7 +72,8 @@ function Clientes() {
                             <div className='options'>
                                 editar
                             </div>
-                        </div><div id='cliente1' className='clientes'>
+                        </div>
+                        <div id='cliente1' className='clientes'>
                             <div className='nome'>Açougue JM</div>
                             <div className='cnpj'>00.000.000/0001-11</div>
                             <div className='options'>
@@ -69,8 +83,52 @@ function Clientes() {
                     </div>
                 </div>
             </section>
+            
+            <div id='sombra' onClick={changeRegClienteContainer}>
+            {/* para escurercer o resto do site ao reg novo cliente */}
+            </div>
             {/* Adicionar Novo */}
-            {/* Listagem */}
+            <section id='containerNovoCliente'>
+                <h2>Registrando cliente</h2>
+                <div id='geral'>
+                    <div id='nome'>
+                        <label>nome</label>
+                        <input type="text" name="" id="" />
+                    </div>
+                    <div id='cnpj'>
+                        <label>CNPJ</label>
+                        <input type="text" name="" id="" />
+                    </div>
+                </div>
+                <div id='contEnderecos' className='contInfosNovoCliente'>
+                    <h3 id='title'>endereços</h3>
+                    <div id='bttShow'>V</div>
+                    <div id='bttNovo'>novo</div>
+                    <div id='inpsEnderecos' className='inpsNovoCliente'>
+                        <input id="" />
+                    </div>
+                </div>
+                <div id='contTelefones' className='contInfosNovoCliente'>
+                    <h3 id='title'>telefones</h3>
+                    <div id='bttShow'>V</div>
+                    <div id='bttNovo'>novo</div>
+                    <div id='inpsTelefones' className='inpsNovoCliente'>
+                        <input id="" />
+                    </div>
+                </div>
+                <div id='contEmails' className='contInfosNovoCliente'>
+                    <h3 id='title'>emails</h3>
+                    <div id='bttShow'>V</div>
+                    <div id='bttNovo'>novo</div>
+                    <div id='inpsEmails' className='inpsNovoCliente'>
+                        <input id="" />
+                    </div>
+                </div>
+                <div id='concForm'>
+                    <button onClick={changeRegClienteContainer}>cancelar</button>
+                    <button onClick={addCliente}>adicionar</button>
+                </div>
+            </section>
         </main>
         </>
     )
--- src/pages/Tecnicos.jsx
+++ src/pages/Tecnicos.jsx
@@ -1,7 +1,48 @@
+import { Link } from 'react-router-dom'
+import navAbrir  from '../assets/navAbrir.png'
+import navFechar  from '../assets/navFechar.png'
+import '../styles/tecnicos.css'
 function Tecnicos() {{
+    async function changeNav() {
+        const sideNav = document.getElementById("sideNavTecnicos")
+        const button = document.getElementById("bttNav")
+
+        if (button.className == "navFechar") {
+            sideNav.style.display = "none"
+            button.className = "navAbrir"
+            button.src = navAbrir
+        } else {
+            sideNav.style.display = "flex"
+            button.className = "navFechar"
+            button.src = navFechar
+        }
+    }
     return (
         <>
-        
+        <nav id='navTecnicos'>
+            <img id="bttNav" src={ navAbrir } alt="" onClick={changeNav} className="navAbrir"/>
+            <Link id="sair" to="/">
+                sair
+            </Link>
+        </nav>
+        <div id="sideNavTecnicos" className="goTo">
+            <Link className="links" to="/home">home</Link>
+            <Link className="links" to="/tecnicos">clientes</Link>
+            <Link className="links" to="/ordens">ordens de serviços</Link>
+        </div>
+        <main id='mainTecnicos'>
+            <section id='sec1'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim asperiores facere aperiam, consequatur beatae commodi sed saepe minima dolorum rem officia alias repellendus vero perspiciatis officiis eum cum. Nostrum, labore.
+            </section>
+            <section id='sec2'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora corporis veniam fuga. Modi, ea eaque consequuntur quisquam veniam voluptatum amet numquam adipisci natus error, nisi voluptatem unde tempore perferendis eveniet!</section>
+            <section id='sec3'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit fugit optio, numquam minima hic minus iusto neque? Est quae, repudiandae iure blanditiis quam esse incidunt quia ex, voluptatem reiciendis ipsa?
+            </section>
+            <section id='sec4'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit fugit optio, numquam minima hic minus iusto neque? Est quae, repudiandae iure blanditiis quam esse incidunt quia ex, voluptatem reiciendis ipsa?
+            </section>
+        </main>
         </>
     )
 }}
