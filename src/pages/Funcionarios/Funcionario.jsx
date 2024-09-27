246caee6a427d8f3247b7368c33b118c586f3c42
LeoMartinez013 <105740782+LeoMartinez013@users.noreply.github.com>
2024-09-27 14:03:40 -0300

    27/09

--- src/pages/Funcionarios/Funcionario.jsx
+++ src/pages/Funcionarios/Funcionario.jsx
@@ -1,23 +1,17 @@
-import Nav from '../../components/public/Nav'
 import '../../styles/funcionario.css'
+import Nav from '../../components/public/Nav'
 import Header from '../../components/public/Header'
 import Down from '../../assets/light/down.png' 
 import Up from '../../assets/light/up.png'
+import { useLocation  } from 'react-router-dom'
+// import { cepAPI } from '../../services/cepAPI'
 
 function Funcionario() {
-    const verContato = (idContato) => {
-        const contato = document.getElementById(`contato${idContato}`)
-        const img = document.getElementById(`img${idContato}`)
-            if (contato.classList == 'contato fechado') {
-            contato.classList = "contato aberto"
-            img.src = Up
-            console.log(`contato ${idContato} aberto`)
-        } else {
-            contato.classList = "contato fechado"
-            img.src = Down
-            console.log(`contato ${idContato} fechado`)
-        }
-    }
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    let idFuncionario = searchParams.get('id');
+    console.log(idFuncionario)
+
     return(
         <div id="pageFuncionario">
             <Header></Header>
@@ -26,9 +20,60 @@ function Funcionario() {
                 <section id='secInfos'>
                     <h2>Funcionário</h2>
                     {/* Infos do funcionarios */}
+                    <div id='contInfosFunc'>
+                        <div>
+                            <label>Nome:</label>
+                            <input type="text" id="" />
+                        </div>
+                        <div>
+                            <label>Email:</label>
+                            <input type="text" id="" />
+                        </div>
+                        <div>
+                            <label>Celular:</label>
+                            <input type="text" id="" />
+                        </div>
+                    </div>
+                    <div id='contEndFunc'>
+                        <div id='cepFunc'>
+                            <label>CEP:</label>
+                            <input type="text" />
+                        </div>
+                        <div id='ruaFunc'>
+                            <label>Rua/logradouro:</label>
+                            <input type="text" />
+                        </div>
+                        <div id='numFunc'>
+                            <label>Nº:</label>
+                            <input type="text" />
+                        </div>
+                        <div id='compFunc'>
+                            <label>Complemento:</label>
+                            <input type="text" />
+                        </div>
+                        <div id='bairroFunc'>
+                            <label>Bairro:</label>
+                            <input type="text" />
+                        </div>
+                        <div id='cidadeFunc'>
+                            <label>Cidade:</label>
+                            <input type="text" />
+                        </div>
+                    </div>
+                    <div id='contControle'>
+                        <button>Cancelar</button>
+                        <button>Salvar</button>
+                        <button>Excluir</button>
+                    </div>
                 </section>
                 <section id='secEspecialidades'>
                     <h2>Especialidades</h2>
+                    <div>
+                        {/* pesquisar */}
+                    </div>
+                    <div>
+                        {/* listagem  */}
+                    </div>
                 </section>
             </main>
         </div>
---
+++ src/services/cepAPI.js
@@ -0,0 +1,5 @@
+// import axios from "axios"
+
+// export const cepAPI = axios.create({
+//     baseURL: "https://brasilapi.com.br/api/cep/v1/"
+// })
