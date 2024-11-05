283535a1b7d62c398a471eb3d825cbfb3e0ca3b8
LeoMartinez013 <105740782+LeoMartinez013@users.noreply.github.com>
2024-11-04 21:28:16 -0300

    04/11

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,23 +3,52 @@ import Header from './public/Header'
 import '../styles/home.css'
 import { useEffect, useState } from 'react'
 import Loading from './public/Loading'
+import { App } from 'antd';
 
 import InfosUser from './Home/InfosUser'
 import FuncsDisponiveis from './Home/FuncsDisponiveis'
 import OrdensAbertas from './Home/OrdensAbertas'
 import OrdensEmAtendimento from './Home/OrdensEmAtendimento'
+import IsOnline from './public/isOnline'
 
 
 function Home() {
+    // Testando notificação
+    const {notification} =  App.useApp
+    const showNotification = () => {
+        notification.info({
+          message: 'Você está offline',
+          description: 'Reconecte-se a internet',
+          placement: 'topLeft',
+        })
+    }
+    // const [api, contextHolder] = notification.useNotification();
+
+    // // const openNotification = (placement: NotificationPlacement) => {
+    // //     api.info({
+    // //     message: `Notification ${placement}`,
+    // //     description:
+    // //         'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+    // //     placement,
+    // //     });
+    // // };
+
+    
+    // ==================================================================
     const [showUsuario, setShowUsuario] = useState(null)
     const [showInfosUser, setShowInfosUser] = useState(true)
     const [usuario, setUsuario] = useState(() => {
         const storedUsuario = sessionStorage.getItem('usuario')
         return storedUsuario ? JSON.parse(storedUsuario) : { cargo: 'dev' }
     })
-
+    useEffect(() => {
+        console.log(navigator.connection)
+        // showNotification()
+    }, [navigator.connection])
     useEffect(() => {
         console.clear()
+        console.log(navigator.connection)
+        // IsOnline
     }, [])
     return (
         <div id='pageHome' className='paginas'>
---
+++ src/components/public/IsOnline.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+const IsOnline = () => {
+    const [networkState, setNetworkState] = useState({
+        isOnline: navigator.onLine,
+        effectiveType: "",
+        downlink: 0,
+        rtt: 0,
+    });
+    useEffect(() => {
+        console.log(networkState)
+    }, [networkState])
+    useEffect(() => {
+        const updateNetState = () => {
+            const connection = navigator.connection;
+            if (connection) {
+                setNetworkState({
+                    isOnline: navigator.onLine,
+                    effectiveType: connection.effectiveType,
+                    downlink: connection.downlink,
+                    rtt: connection.rtt,
+                });
+            }
+        };
+        window.addEventListener("load", updateNetState);
+        window.addEventListener("online", updateNetState);
+        window.addEventListener("offline", updateNetState);
+
+        return () => {
+            window.removeEventListener("load", updateNetState);
+            window.removeEventListener("online", updateNetState);
+            window.removeEventListener("offline", updateNetState);
+        };
+    }, []);
+
+    return networkState;
+};
+
+export default IsOnline;
