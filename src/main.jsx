479dcabc769272a1c691f6f7e1bb90b810c9c31c
LeoMartinez013 <105740782+LeoMartinez013@users.noreply.github.com>
2025-03-16 00:38:49 -0300

    Sem SCSS por enquanto

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,25 @@
-import { StrictMode } from 'react'
+import { lazy, StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import AuthProvider from '@context/authContext.jsx';
-import AppRoutes from "./router/routes.jsx";
+// import AppRoutes from "./router/routes.jsx"; 
 import { PreferenciasProvider } from '@context/PreferenciasContext.jsx';
 import './index.css'
 import { UsuarioProvider } from './context/UsuarioContext.jsx';
+import Loading from '@components/public/Loading.jsx'
+
+const AppRoutes = lazy(() => import('./router/routes.jsx'))
 
 function App() {
     return (
         <AuthProvider>
             <UsuarioProvider>
               <PreferenciasProvider>
-                  <AppRoutes />
+                <div>
+                  {/* O Suspense exibe um fallback enquanto o componente é carregado */}
+                  <Suspense fallback={<Loading texto={"carregando SIGOS"}/>}>
+                    <AppRoutes />
+                  </Suspense>
+                </div>
               </PreferenciasProvider>
             </UsuarioProvider>
         </AuthProvider>
