e73495520473dce8d0c6478b5ba2469e88638875
LeoMartinez013 <105740782+LeoMartinez013@users.noreply.github.com>
2025-03-24 14:54:46 -0300

    Update Mapa.jsx

--- src/components/public/Mapa.jsx
+++ src/components/public/Mapa.jsx
@@ -4,32 +4,33 @@ import { getGoogleMapsToken } from "@services/mapsAPI";
 
 export default function Mapa({ endereco }) {
     const token = getGoogleMapsToken();
-    const formatEndereco = `${endereco.logradouro}, ${endereco.numero}, ${endereco.cidade}, ${endereco.uf}, Brasil`;
+    const formatEndereco = `${endereco.logradouro}, ${endereco.numero}, CEP ${endereco.cep}, ${endereco.cidade}, ${endereco.uf}, Brasil`;
+
     const [position, setPosition] = useState(null);
     const [mapsLoaded, setMapsLoaded] = useState(false);
     useEffect(() => {
-    if (
-      mapsLoaded &&
-      window.google &&
-      window.google.maps &&
-      typeof window.google.maps.Geocoder === "function"
-    ) {
-      const geocoder = new window.google.maps.Geocoder();
-      geocoder.geocode({ address: formatEndereco }, (results, status) => {
-        if (status === "OK" && results[0]) {
-          setPosition(results[0].geometry.location);
-        } else {
-          console.error("Geocode não obteve sucesso: ", status);
+        if (
+            mapsLoaded &&
+            window.google &&
+            window.google.maps &&
+            typeof window.google.maps.Geocoder === "function"
+        ) {
+            const geocoder = new window.google.maps.Geocoder();
+            geocoder.geocode({ address: formatEndereco }, (results, status) => {
+                if (status === "OK" && results[0]) {
+                    setPosition(results[0].geometry.location);
+                } else {
+                    console.error("Geocode não obteve sucesso: ", status);
+                }
+            });
         }
-      });
-    }
-  }, [mapsLoaded, formatEndereco])
+    }, [mapsLoaded, formatEndereco])
 
     return (
         <APIProvider apiKey={token} onLoad={() => (
-                console.log('Maps API has loaded.'),
-                setMapsLoaded(true)
-            )}>
+            console.log('Maps API has loaded.'),
+            setMapsLoaded(true)
+        )}>
             <div id="contMapa" style={{ height: "20rem", width: "20rem" }}>
                 {position ? (
                     <Map
